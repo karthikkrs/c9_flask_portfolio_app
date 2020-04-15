0cc5659d379de38f0ef6269721714cb70774b9f9
Karthik R Sundar <karthik.sundar28@gmail.com>
2020-04-15 18:22:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def profile(name):
 	return render_template('index.html', name=new_name)
 
 
-app.route('/add_numbers', methods=['GET','POST'])
+@app.route('/add_numbers', methods=['GET','POST'])
 def add_numbers_post():
 	  # --> ['5', '6', '8']
 	  # print(type(request.form['text']))
